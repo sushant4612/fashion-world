0e6b4cc825cb6127eb77d0a9f7e36ea2cceb2513
SUSHANT PATHARE <sushantsp14@gmail.com>
2024-12-21 18:31:08 +0530

    Backend Structure Created

---
+++ backend/src/index.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+
+//App Config
+dotenv.config()
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+//middlewares
+app.use(express.json());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}));
+
+app.get("/", (req, res) => {
+    res.send("Api Working")
+});
+
+app.listen(PORT, () => {
+    console.log(`App is listening at port http://localhost:${PORT} `);
+})
