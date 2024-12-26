3b9561b7c4ee9e419903cc73e0cfb626fa5c871e
SUSHANT PATHARE <sushantsp14@gmail.com>
2024-12-26 13:08:10 +0530

    Changed Register to use ApiResponse and ApiError

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -3,12 +3,14 @@ import { CreateUserDto, CreateUserSchema } from "../dtos/CreateUser.dto";
 import userModel from "../models/User.model";
 import bcrypt from 'bcrypt'
 import jwt from "jsonwebtoken";
+import ApiError from "../utils/ApiError";
+import ApiResponse from "../utils/ApiResponse";
 
 // Create Token
 const createToken = (id: string): string => {
     const secret = process.env.JWT_SECRET
     if(!secret){
-        throw new Error("JWT_SECRET is not defined in environment variables")
+        throw new ApiError(404, "JWT_SECRET is not defined in environment variables")
     }
     return jwt.sign({id}, secret);
 }
@@ -20,10 +22,7 @@ const registerUser = async (req: Request<{},{}, CreateUserDto>, res: Response): 
 
         const exists = await userModel.findOne({email});
         if(exists){
-            return res.status(400).json({
-                success: false,
-                message: "User already exists"
-            })
+            throw new ApiError(400,"User already exists");
         }
 
         const salt = await bcrypt.genSalt(10);
@@ -39,17 +38,11 @@ const registerUser = async (req: Request<{},{}, CreateUserDto>, res: Response): 
 
         const token = createToken(user._id);
 
-        return res.status(200).json({
-            success: true,
-            token
-        })
+        return res.status(200).json(new ApiResponse(200, token, "User registered successfully "))
 
     } catch (error: any) {
         console.log(error);
-        return res.json(404).json({
-            success: false,
-            message: error.message
-        })
+        throw new ApiError(404, error.message)
     }
 }
 
