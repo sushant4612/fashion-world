c5a55570f466908c4189dac7467040044a08c956
SUSHANT PATHARE <sushantsp14@gmail.com>
2025-01-04 16:12:34 +0530

    Place Order Razorpay added

--- backend/src/controllers/order.controller.ts
+++ backend/src/controllers/order.controller.ts
@@ -3,8 +3,16 @@ import PlaceOrderDto from "../dtos/PlaceOrder.dto";
 import orderModel from "../models/Order.model";
 import userModel from "../models/User.model";
 import ApiResponse from "../utils/ApiResponse";
+import Razorpay from "razorpay"
+import ApiError from "../utils/ApiError";
 
+const currency = 'inr';
 
+// gateway initialize
+const razorpayInstance = new Razorpay({
+    key_id: process.env.RAZORPAY_KEY_ID || '',
+    key_secret: process.env.RAZORPAY_KEY_SECRET
+})
 
 //Placing order using COD
 const placeOrder = async (req: Request<{},{},PlaceOrderDto>, res: Response, next: NextFunction): Promise<any> => {
@@ -33,9 +41,35 @@ const placeOrder = async (req: Request<{},{},PlaceOrderDto>, res: Response, next
 }
 
 //Placing Order using razorpay
-const placeOrderRazorpay =  async (req: Request, res: Response, next: NextFunction): Promise<any> => {
+const placeOrderRazorpay =  async (req: Request<{},{},PlaceOrderDto>, res: Response, next: NextFunction): Promise<any> => {
     try {
-        
+        const {userId, items, amount, address} = req.body;
+
+        const orderData = {
+            userId,
+            items,
+            amount,
+            address,
+            paymentMethod: "Razorpay",
+            payment:false,
+            date: Date.now()
+        }
+
+        const newOrder: any = new orderModel(orderData);
+        await newOrder.save();
+
+        const options = {
+            amount: amount * 100 ,
+            currency: currency.toUpperCase(),
+            receipt: newOrder._id.toString()
+        }
+
+        await razorpayInstance.orders.create(options, (error, order) => {
+            if(error){
+                throw new ApiError(400, "Error in Razorpay", [error]);
+            }
+        })
+
     } catch (error) {
         next(error);
     }
