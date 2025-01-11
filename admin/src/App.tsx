22ddf23169964ee20c8a4c0a1694734623124b1f
SUSHANT PATHARE <sushantsp14@gmail.com>
2025-01-11 16:28:32 +0530

    App page Configure

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -1,26 +1,44 @@
 
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import Login from './components/Login'
 import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
 import { Route, Routes } from 'react-router-dom';
 import Add from './pages/Add';
+import { ToastContainer } from 'react-toastify';
 
 export const backendUrl = import.meta.env.VITE_BACKEND_URL;
+export const currency = '$';
 
 function App() {
-  const [token, setToken] = useState<string>('')
+  const [token, setToken] = useState<string>(localStorage.getItem('token') || ""); 
+
+  useEffect(() => {
+    localStorage.setItem('token', token);
+  },[token])
+
   return (
-    <div className='bgColor'>
-      <Navbar setToken={setToken}/>
-      {/* <Login setToken={setToken}/> */}
-      <Sidebar/>
-      <div className='w-[70%] mx-auto ml-[max(5vw,25px) my-18] text-gray-600 text-base'>
-        <Routes>
-            <Route path='/add' element={<Add  token={token}/>}/>
-        </Routes>
-      </div>
+    <div className='bg-gray-50 min-h-screen'>
+      <ToastContainer/>
+      {token === "" ? (
+        <Login setToken={setToken}/>
+      ) : (
+        <>
+          <Navbar setToken={setToken}/>
+          <hr />
+          <div className='flex w-full'>
+              <Sidebar/>
+              <div className='w-[70%] mx-auto ml-[max(5vw,25px) my-18] text-gray-600 text-base'>
+              <Routes>
+                <Route path='/add' element={<Add token={token} />} />
+                {/* <Route path='/list' element={<List token={token} />} />
+                <Route path='/orders' element={<Order token={token} />} /> */}
+              </Routes>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   )
 }
