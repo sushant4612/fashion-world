8b09ab4cae9a8e546d8248decc2dabe98060af9e
SUSHANT PATHARE <sushantsp14@gmail.com>
2025-01-11 17:10:47 +0530

    Navbar img added

--- admin/src/components/Navbar.tsx
+++ admin/src/components/Navbar.tsx
@@ -8,10 +8,9 @@ interface NavbarProps {
 const Navbar: React.FC<NavbarProps> = ({setToken}) => {
     return (
         <div className="flex items-center py-2 px-[4%] justify-between">
-            <div className="flex items-center">
+            <div className="flex items-center gap-4">
                 <img style={{ width: 'max(7%, 40px)' }} src={assets.logo} alt="" />
-                <p className="font-semibold">Flowerrrr World</p>
-                <img style={{ width: 'max(7%, 40px)' }} src={assets.logo} alt="" />
+                <p className="logo font-bold text-3xl">Fashion World</p>
             </div>
             <button onClick={() => setToken('')} className="bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm">Logout</button>
         </div>
