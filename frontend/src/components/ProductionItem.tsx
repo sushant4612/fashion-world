59c4ec0e5f069715ea2da348ca8e886c45ce3403
SUSHANT PATHARE <sushantsp14@gmail.com>
2025-01-17 19:11:06 +0530

    ProductItem component Added

--- frontend/src/components/ProductionItem.tsx
+++ frontend/src/components/ProductionItem.tsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ShopContext } from '../context/ShopContext';
+import { Link } from 'react-router-dom';
 
+interface ProductItemProps {
+  id: string;
+  image: string[];
+  name: string;
+  price: number
+}
 
+export const ProductionItem:React.FC<ProductItemProps> = ({id, image, name, price}) => {
+  const {currency} = useContext(ShopContext);
 
-export const ProductionItem:React.FC = () => {
   return (
-    <div>
-      
-    </div>
+    <Link to={`/product/${id}`} className='text-gray-700 cursor-pointer'>
+      <div className='overflow-hidden group'>
+        <img className='group-hover:scale-110 transition-transform duration-300 ease-in-out' src={image[0]} alt={name}/>
+        <p className='pt-3 pb-1 text-sm font-semibold'>{name}</p>
+        <p className='text-sm font-medium'>{currency} {price}</p>
+      </div>
+    </Link>
   )
 }
