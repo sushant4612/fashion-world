ba2afd6ded00ab565e7ccdbcf4cdd591acfce044
SUSHANT PATHARE <sushantsp14@gmail.com>
2025-01-19 19:01:03 +0530

    OurPolicy Component Added

--- frontend/src/components/OurPolicy.tsx
+++ frontend/src/components/OurPolicy.tsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React from 'react';
+import { assets } from '../assets/assets';
 
-const OurPolicy:React.FC = () => {
+const OurPolicy: React.FC = () => {
   return (
-    <div>OurPolicy</div>
-  )
-}
+    <div className="flex flex-col sm:flex-row justify-around items-center gap-12 sm:gap-6 text-center py-20  text-gray-700">
+      <div className="p-6 bg-white shadow-lg rounded-lg hover:shadow-xl transition-shadow duration-300 ease-in-out transform hover:scale-105">
+        <img 
+          src={assets.exchange_icon} 
+          className="w-16 h-16 m-auto mb-5 rounded-full bg-blue-100 p-2" 
+          alt="Exchange Policy Icon" 
+        />
+        <p className="font-semibold text-lg">Easy Exchange Policy</p>
+        <p className="text-gray-500 mt-2 text-sm">We offer a hassle-free exchange policy for all our customers.</p>
+      </div>
+      <div className="p-6 bg-white shadow-lg rounded-lg hover:shadow-xl transition-shadow duration-300 ease-in-out transform hover:scale-105">
+        <img 
+          src={assets.quality_icon} 
+          className="w-16 h-16 m-auto mb-5 rounded-full bg-green-100 p-2" 
+          alt="Quality Icon" 
+        />
+        <p className="font-semibold text-lg">7 Days Return Policy</p>
+        <p className="text-gray-500 mt-2 text-sm">Enjoy a 7-day free return policy on all purchases.</p>
+      </div>
+      <div className="p-6 bg-white shadow-lg rounded-lg hover:shadow-xl transition-shadow duration-300 ease-in-out transform hover:scale-105">
+        <img 
+          src={assets.support_img} 
+          className="w-16 h-16 m-auto mb-5 rounded-full bg-yellow-100 p-2" 
+          alt="Customer Support Icon" 
+        />
+        <p className="font-semibold text-lg">24/7 Customer Support</p>
+        <p className="text-gray-500 mt-2 text-sm">Our support team is available 24/7 to assist you.</p>
+      </div>
+    </div>
+  );
+};
 
-export default OurPolicy
+export default OurPolicy;
--- frontend/src/components/ProductItem.tsx
+++ frontend/src/components/ProductItem.tsx
@@ -16,8 +16,8 @@ export const ProductItem:React.FC<ProductItemProps> = ({id, image, name, price})
     <Link to={`/product/${id}`} className='text-gray-700 cursor-pointer'>
       <div className='rounded-md overflow-hidden group'>
         <img className='group-hover:scale-110 transition-transform duration-300 ease-in-out' src={image[0]} alt={name}/>
-        <p className='pt-3 pb-1 text-sm font-semibold'>{name}</p>
-        <p className='text-sm font-medium'>{currency} {price}</p>
+        <p className='pt-3 p-1 pb-1 text-sm font-semibold'>{name}</p>
+        <p className='p-1 text-sm font-medium'>{currency} {price}</p>
       </div>
     </Link>
   )
