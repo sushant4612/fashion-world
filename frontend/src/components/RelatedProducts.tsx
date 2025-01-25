0641b4f117ef5d2a63b50d13a4485563d0bf98ed
SUSHANT PATHARE <sushantsp14@gmail.com>
2025-01-25 10:19:04 +0530

    Related Product component added

--- frontend/src/components/RelatedProducts.tsx
+++ frontend/src/components/RelatedProducts.tsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react';
+import { ShopContext } from '../context/ShopContext';
+import Title from './Title';
 
-const RelatedProducts:React.FC = () => {
-  return (
-    <div>RelatedProducts</div>
-  )
+import { Product } from '../context/ShopContext';
+import { ProductItem } from './ProductItem';
+
+interface RelatedProductsProps {
+  category: string;
+  subCategory: string;
 }
 
-export default RelatedProducts
+const RelatedProducts: React.FC<RelatedProductsProps> = ({ category, subCategory }) => {
+  const { products } = useContext(ShopContext) as { products: Product[] };
+  const [related, setRelated] = useState<Product[]>([]);
+
+  useEffect(() => {
+    if (products.length > 0) {
+      let productsCopy = [...products];
+
+      productsCopy = productsCopy.filter((item) => category === item.category);
+      productsCopy = productsCopy.filter((item) => subCategory === item.subCategory);
+
+      setRelated(productsCopy.slice(0, 5));
+    }
+  }, [products, category, subCategory]);
+
+  return (
+    <div className="my-24">
+      <div className="text-center text-3xl py-2">
+        <Title text1="RELATED" text2="PRODUCTS" />
+      </div>
+
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6">
+        {related.map((item) => (
+          <ProductItem
+            key={item._id}
+            id={item._id}
+            name={item.name}
+            price={item.price}
+            image={item.image}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default RelatedProducts;
